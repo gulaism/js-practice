4948e14360ea1970b50b9e3badc37d815ba8564b
Gulay <gulaymovlamova@gmail.com>
2024-03-04 18:43:52 +0400

    initial

---
+++ new.js
@@ -0,0 +1,29 @@
+function repeatText(text, repeat){
+    for(let i = 0; i < repeat; i++) console.log(`${text} - ${i+1}`)
+}
+repeatText('Salam', 10)
+
+
+
+function area(a, b){
+    console.log(a * b)
+}
+function perimetr(a, b){
+    console.log(2*(a+b))
+}
+area(10, 50)
+perimetr(10,50)
+
+
+
+function zerAtma(){
+    console.log(Math.floor((Math.random()*6) + 1));
+}
+zerAtma()
+
+
+
+function toplama(a, b){
+    console.log(a + b)
+}
+toplama(19, 1)
